49217a493fc2e6c36e88e04511ddd016211060ff
mramshaw <mramshaw@alumni.concordia.ca>
2019-11-30 14:55:25 +0000

    Updates

---
+++ precache-manifest.9881ec537ca6d2a993da485aac77ced3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e1bd2c1f9c368ca51bce05d8c87f0b",
+    "url": "/tic-tac-toe-jim/index.html"
+  },
+  {
+    "revision": "9dc975d105c20d3c268f",
+    "url": "/tic-tac-toe-jim/static/css/main.a51b68fb.chunk.css"
+  },
+  {
+    "revision": "b6f1a9f8c6d27229bf60",
+    "url": "/tic-tac-toe-jim/static/js/2.43056cf4.chunk.js"
+  },
+  {
+    "revision": "9dc975d105c20d3c268f",
+    "url": "/tic-tac-toe-jim/static/js/main.edd362b8.chunk.js"
+  },
+  {
+    "revision": "44299ed62af7eb9ae603",
+    "url": "/tic-tac-toe-jim/static/js/runtime-main.6cb23be1.js"
+  }
+]);
